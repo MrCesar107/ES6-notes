63b3a9a985b6871a9796a929838bbe46a2412634
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2020-08-24 23:47:45 -0500

    ES7 new features

---
+++ src/es7/index.js
@@ -0,0 +1,18 @@
+// Includes
+
+let numbers = [1, 2, 3, 6, 7, 5, 9];
+
+if(numbers.includes(7))
+  console.log('Si hay un valor 7');
+else
+  console.log('No se encuentra :(');
+
+// More information about includes method
+// https://github.com/FiliSantillan/Bits/blob/master/BitSnack/includes/includes.md
+
+// Exponents
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
